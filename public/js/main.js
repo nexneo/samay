a91048392b985afde5b7ed042ca561791c0ea40f
Niket Patel <nexneo@gmail.com>
2013-04-21 01:04:18 -0700

    Project listing on web

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,20 @@
+function SamayHomeCtrl($scope, $http){
+	$http.get("/app.json").success(function(data){
+		var projects = [];
+		for (var i = data.length - 1; i >= 0; i--) {
+			project = data[i]['project'];
+			project['entries'] = data[i]['entries'];
+			projects[i] = project;
+		}
+		$scope.projects = projects;
+		$scope.activate($scope.projects[0])
+	});
 
+	$scope.activate = function(project) {
+		angular.forEach($scope.projects, function(p){
+			p.activecls = "";
+		})
+		project.activecls = "active";
+		$scope.activeProject = project;
+	};
+}
