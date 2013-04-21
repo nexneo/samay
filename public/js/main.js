5609a2ee384f318aa8f1075ab49323f3c8483024
Niket Patel <nexneo@gmail.com>
2013-04-21 16:33:28 -0700

    Duration calculation and date formatting using moment.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,40 @@
+angular.module('samay.filter', []).
+filter('hoursmins', function() {
+	return function(input){
+		var hh = Math.floor(input.asHours()),
+			mm = "0" + input.minutes();
+		
+		return hh+":"+mm.substr(-2);
+	};
+}).
+filter('prettydate', function() {
+	return function(input){
+		return moment.unix(input).format("MMM Do, hA");
+	};
+});
+
+
+
+angular.module('samay', ['samay.filter']);
+
+
 function SamayHomeCtrl($scope, $http){
+	function DecoEntries(entries) {
+		for (var i = entries.length - 1; i >= 0; i--) {
+			entries[i]['hours'] = moment.duration(entries[i].duration/1000000);
+		}
+		return entries;
+	}
+
+	$scope.notEmpty = function (str) {
+		return str != "";
+	}
+
 	$http.get("/app.json").success(function(data){
 		var projects = [];
 		for (var i = data.length - 1; i >= 0; i--) {
 			project = data[i]['project'];
-			project['entries'] = data[i]['entries'];
+			project['entries'] = DecoEntries(data[i]['entries']);
 			projects[i] = project;
 		}
 		$scope.projects = projects;
@@ -17,4 +48,16 @@ function SamayHomeCtrl($scope, $http){
 		project.activecls = "active";
 		$scope.activeProject = project;
 	};
+
+	$scope.totalHours = function (){
+		if($scope.activeProject === undefined){
+			return moment.duration(0);
+		}
+		
+		var ret = 0;
+		angular.forEach($scope.activeProject.entries, function(e){
+			ret += e.duration;
+		});
+		return moment.duration(ret/1000000)
+	}
 }
